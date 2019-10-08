5544d30afa9ea4a72721ae9bac43a1aa1486c506
A <A@As-MacBook-Air.local>
2019-10-08 19:21:39 +0200

    cleaned up comments from pdf

--- 2.visualize/pair_plot.py
+++ 2.visualize/pair_plot.py
@@ -1,2 +1,2 @@
 ## displays a pair plot or scatter plot matrix
-## From this visualization, what features are you going to use for your logistic regression?
+## From this visualization, what features are you going to use for your logistic regression ?
--- 3.logreg/logreg_predict.py
+++ 3.logreg/logreg_predict.py
@@ -1,6 +1,6 @@
 ## takes as a parameter dataset_test.csv
 ## and a file containing the weights trained by previous program
-## generates a prediction file houses.csv formatted as follows:
+## generates a prediction file houses.csv in format:
 
 ## $> cat houses.csv
 ## Index,Hogwarts House
