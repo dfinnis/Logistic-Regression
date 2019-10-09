7ece476bb4959271acaa5d4b0dbe51e2fc3a66e8
Anya SCHUKIN <aschukin@e2r6p18.42.fr>
2019-10-09 11:20:43 +0200

    fix names

---
+++ logreg/logreg_train.py
@@ -0,0 +1,9 @@
+## takes as a parameter dataset_train.csv
+## uses gradient descent to minimize error
+## generates a file containing the weights that will be used for the prediction
+
+def main():
+    print("Hello world!")
+
+if __name__ == '__main__':
+    main()
---
+++ visualize/scatter_plot.py
@@ -0,0 +1,8 @@
+## displays a scatter plot answering the next question :
+## What are the two features that are similar ?
+
+def main():
+    print("Hello world!")
+
+if __name__ == '__main__':
+    main()
