6ab1d67c91f12ae5e2317a89b4f5c8e2495373cd
Anya SCHUKIN <aschukin@e2r1p8.42.fr>
2019-10-15 16:02:24 +0200

    seems to make good predictions but messy

--- 3.logreg/logreg_train.py
+++ 3.logreg/logreg_train.py
@@ -4,6 +4,12 @@ import tools as tools
 sys.path.append('../1.analyze/')
 import describe as describe
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from Logistic_Regression import fit, cost, predict, sigmoid
+
 def write_weights(weights):
     try:
         file = open('../data/weights.csv', 'w')            
@@ -22,17 +28,50 @@ def normalize(data):
             normed[column] = (normed[column] - mean) / std
     return normed
 
+def filter_House(housename, y):
+    house = np.array([housename])
+    return y == house
+
 def preprocess(data):
     data = data.drop(columns=['Arithmancy', 'Defense Against the Dark Arts', 'Care of Magical Creatures'])
     data = data.dropna()
     normed = normalize(data)
-    print(data) ###
-    print('oh hi') ########
-    print(normed) #######
-    return data, normed
+
+    X = normed.loc[:, 'History of Magic':'Transfiguration']
+    ones = np.ones([X.shape[0],1])
+    X = np.concatenate((ones, X), axis=1)
+    y = np.array(data.loc[:,'Hogwarts House']).reshape(1333,1)
+
+    housename = 'Gryffindor'
+    y = filter_House(housename, y).astype(int)
+
+    return normed, X, y
+    # return data, normed
 
 def train(data):
-    data, normed = preprocess(data)
+    # data, normed = preprocess(data)
+    normed, X, y = preprocess(data)
+
+    alpha = 0.02
+    num_iters = 100000
+    theta = np.zeros(3).reshape(3, 1)
+    theta, J_history = fit(X, y, theta, alpha, num_iters)
+    
+    # print(theta, J_history[-1])
+
+    # plt.figure()
+    # ax = plt.axes()
+    # ax.plot(J_history)
+    # plt.show()
+
+    p = predict(X, theta)
+    # np.set_printoptions(threshold=sys.maxsize)    
+    print(p)
+    # accuracy = y - p
+    # score = np.where(accuracy == 0)[1].size
+    # print(score)
+
+
     ### pump some iron ###
     weights = 'Thanks for stopping by!' ###
     return weights
