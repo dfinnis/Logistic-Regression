ac9ad62eb3739d8ef2af9bcb32b412b58599d8a0
A <A@As-MacBook-Air.local>
2019-10-20 20:04:42 +0200

    refacto/fixed describe to correct output

--- 1.analyze/describe.py
+++ 1.analyze/describe.py
@@ -6,46 +6,49 @@ sys.path.append('../tools/')
 import tools as tools
 
 def std_dev(feature, mean, count):
-    total = 0
+    total = 0.0
     for value in feature:
         if str(value) != 'nan':
-            diff = value - mean
-            square = diff * diff
-            total += square
-    mean_sq = total / count
+            total += ((value - mean) ** 2)
+    mean_sq = total / (count - 1)
     std_dev = math.sqrt(mean_sq)
     return std_dev
+
+def find_quart(feature, count, quart = 1):
+    index = (count - 1) * (quart * 0.25)
+    floor = math.floor(index)
+    if floor == index:
+        return feature[int(index)]
+    return (feature[floor] + (feature[floor + 1] - feature[floor]) * (index - floor))
 
 def find_features(data):
     try:
         features = pd.DataFrame({'': ['Count', 'Mean ', 'Std  ', 'Min  ', '25%  ', '50%  ', '75%  ', 'Max  ']})
         col = 0
         for column in data.columns:
-            # if col == 4:
-            #     ### deal with age
-            # if col == 5:
-            #     ### deal with left/right handed
+            # if col == 4: ### deal with age
+            # if col == 5: ### deal with left/right handed
             if col > 5:
                 count = 0
                 total = 0
                 feature = np.array(data[column], dtype='float64')
                 feature = np.sort(feature)
-                set = 0
+                min_set = 0
                 for value in feature:
                     if str(value) != 'nan':
                         count += 1
                         total += value
-                        if set == 0:
+                        if min_set == 0:
                             minimum = value
                             maximum = value
-                            set = 1
+                            min_set = 1
                         else:
                             maximum = value
                 mean = total / count
                 std = std_dev(feature, mean, count)
-                quarter = feature[round(count/4)]
-                half = feature[round(count/2)]
-                three_quarter = feature[round((count/4)*3)]
+                quarter = find_quart(feature, count, 1)
+                half = find_quart(feature, count, 2)
+                three_quarter = find_quart(feature, count, 3)
 
                 features[column] = np.array([count, mean, std, minimum, quarter, half, three_quarter, maximum])
             col += 1
